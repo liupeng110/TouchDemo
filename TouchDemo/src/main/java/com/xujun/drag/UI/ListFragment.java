62ad085fa1bf8591b157c384d04a6d0bd859f02d
liupeng <717219917@qq.com>
2017-12-26 15:59:46 +0800

    加注释

--- TouchDemo/src/main/java/com/xujun/drag/UI/ListFragment.java
+++ TouchDemo/src/main/java/com/xujun/drag/UI/ListFragment.java
@@ -45,7 +45,7 @@ public class ListFragment extends BaseFragment {
 
     ScrollView mNoHorizontalScrollView;
 
-    private int mSize = 4;
+    private int mSize = 4;  
 
     private int mScrollY;
     private int mScrollX;
